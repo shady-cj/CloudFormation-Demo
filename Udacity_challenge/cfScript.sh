b6fa88ad7d9d77e08f1ca336c4ceb5f4eb11ed08
shady-cj <petersp2000@gmail.com>
2022-06-17 20:47:19 +0100

    Challenge 3 locked in

--- Udacity_challenge/cfScript.sh
+++ Udacity_challenge/cfScript.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-aws cloudformation create-stack --stack-name $1 us-east-1 --template-body file://$2 --parameters file://$3
+aws cloudformation create-stack --stack-name $1 --region us-east-1 --template-body file://$2 --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
